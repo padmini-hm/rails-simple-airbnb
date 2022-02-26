278e64756fa54260227ac9f42ebfc27eec3b6d97
Padmini Hiriyur <padmini.hm@gmail.com>
2022-02-26 20:45:20 +1100

    modified and added files for rails-simple-airbnb

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,4 @@
 class Flat < ApplicationRecord
+  validates :name, :presence => true
+  validates :number_of_guests, :inclusion => { :in => [1,2,3,4,5] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+
+  Flat.create(
+    name: Faker::Address.city,
+    address: Faker::Address.street_address,
+    description: Faker::Quotes::Rajnikanth.joke,
+    price_per_night: 75,
+    number_of_guests: 3
+  )
+end
